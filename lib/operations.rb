ef8f3469d04498483ea01aad80ab1539ce2d6516
Ethan Ryan <ethanryan@gmail.com>
2017-02-27 19:04:46 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
+  #conditional ? action_if_true : action_if_false
+  (speed > 60 || speed < 40) ? true : false # if speed is greater than 60 or less than 40, it is unsafe
+end #end of method
 
-end
-
-
+#syntax for ternary operator, to use if..else on one line:
+#conditional ? action_if_true : action_if_false
+#note: only use if it's a simple if..else, and don't use when elsifs are involved.
 
 def not_safe?(speed)
-	
-end
-	
-
-
+  #conditional ? action_if_true : action_if_false
+  (speed > 60 || speed < 40) ? true : false # if speed is greater than 60 or less than 40, it is unsafe
+end #end of method
